a75be79dab0b9a53c1865599a3f62c2e48cc46ed
lucas bevilaqua araujo de oliveira <laobevilaqua@outlook.com>
2025-11-27 01:55:32 -0300

    ci: create enviroment template

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  auth0: { 
+    clientId: '__AUTH0_CLIENT_ID__',
+    domain: '__AUTH0_DOMAIN__',  
+  },
+  firebase: {
+    apiKey: '__FIREBASE_API_KEY__',
+    authDomain: '__FIREBASE_AUTH_DOMAIN__',
+    projectId: '__FIREBASE_PROJECT_ID__',
+    storageBucket: '__FIREBASE_STORAGE_BUCKET__',
+    messagingSenderId: '__FIREBASE_MESSAGING_SENDER_ID__',
+    appId: '__FIREBASE_APP_ID__',
+    databaseURL: '__FIREBASE_DATABASE_URL__',
+  },
+};
